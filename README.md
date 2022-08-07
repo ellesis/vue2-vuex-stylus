f8fb2c4837e520a7b93673ee48b6b17870376473
Lisa Han <ellesis@gmail.com>
2022-08-07 07:34:52 -0800

    Add button and popup vue-dialog-drag

--- README.md
+++ README.md
@@ -1,27 +1,39 @@
-# vue2-jest
+# vue2-stylus-vuex
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
-
 pug
+
+## Install Library
+
+1. ElementUI
+   npm i element-ui -S
+
+2. vue-dialog-drag
+   npm install vue-dialog-drag --save
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" />
     <div>
       <p>
         If Element is successfully added to this project, you'll see an
@@ -8,29 +8,57 @@
         below
       </p>
       <el-button>el-button</el-button>
+      <!-- Dialog -->
+      <div>
+        <button v-if="!showDialog" @click="showDialog = true">Show Dialog</button>
+      </div>
+      <div style="width: 30%">
+        <dialog-drag v-if="showDialog" id="dialog-1" :options="options" @close="closedDialog"> </dialog-drag>
+      </div>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import DialogDrag from 'vue-dialog-drag'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+      DialogDrag
+    },
+    data() {
+      return {
+        showDialog: false,
+        options: {
+          buttonPin: false,
+          x: 20,
+          y: 200,
+          width: 500,
+          height: 400
+        }
+      }
+    },
+    methods: {
+      closedDialog() {
+        this.showDialog = false
+      }
+    }
   }
-}
 </script>
 
+<style src="vue-dialog-drag/dist/vue-dialog-drag.css"></style>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
