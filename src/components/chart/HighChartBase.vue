7cfddfc56d5a99be9f27b86774bd24b48df9d304
Lisa Han <ellesis@gmail.com>
2022-10-22 14:49:14 -0700

    Add HighChartBase component

---
+++ src/components/chart/HighChartBase.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <highcharts :options="chartOptions"></highcharts>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        chartOptions: {}
+      }
+    }
+  }
+</script>
